2fda9df46a57fb11f75727a9ec9772678c9d0a43
Silvia Taliana <silviataliana2@gmail.com>
2021-02-25 14:04:19 +1030

    connecting html files to backend

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+// Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
