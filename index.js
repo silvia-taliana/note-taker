851839f41cf5ccc6baaa58fcd955825cac3a0393
Silvia Taliana <silviataliana2@gmail.com>
2021-02-26 13:51:13 +1030

    adding function to read db.json file

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+const db = "./db/db.json";
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// reading db.json file
+fs.readFile(db, 'utf8', (err, data) => {
+    if (err) {
+        console.log("Error");
+    }
+    else {
+        console.log(data);
+    }
+});
 
 // Routes
 app.get('/notes', (req, res) => {
