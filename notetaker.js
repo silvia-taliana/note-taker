46c8a12bd6ae5ba6811dc4836e26efdc4e917149
Silvia Taliana <silviataliana2@gmail.com>
2021-02-26 18:39:41 +1030

    console log new note and fixed css

--- notetaker.js
+++ notetaker.js
@@ -4,18 +4,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const fs = require('fs');
 const db = "./db/db.json";
+let note = [];
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // reading db.json file
-fs.readFile(db, 'utf8', (err, data) => {
+fs.readFile(db, (err, data) => {
     if (err) {
         console.log("Error");
     }
     else {
-        console.log(data);
+        note = JSON.parse(data);
+        // console.log(note);
     }
 });
 
@@ -28,5 +31,17 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
+app.get('/api/notes', (req, res) => {
+    res.json(note);
+    // console.log(res.json(JSON.parse(note)));
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    note.push(newNote);
+    console.log(note);
+});
+
 // Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
